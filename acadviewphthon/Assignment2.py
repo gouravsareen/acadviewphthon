6d6d2d2447bd90484ca66e1963aae7ce7ef0105d
gouravsareen <gouravsareen0112@gmail.com>
2018-06-13 15:51:14 +0530

    assignment2 done completely

---
+++ acadviewphthon/Assignment2.py
@@ -0,0 +1,36 @@
+#Q1:-Print anything you want on screen.
+print("My name is gourav sareen.I choose Acadview for my summer training")
+
+#Q2:-Join two strings using "+".E.g."Acad"+"View"
+a="Acad"
+b="View"
+print(a+b)
+
+#Q3:-Take the input of three variables x,y and z.Print their values on screen.
+x=input("enter your name: ")
+y=input("enter your age: ")
+z=input("enter your course: ")
+print(x,y,z)
+
+#Q4:-Print "Let's get started" on screen.
+
+print('"Let\'s get started"')
+
+#Q5:-Print the following values using placeholders.
+                #s="Acadview"
+                #course="Python"
+                #fees=5000
+
+s="Acadview"
+course="Python"
+fees=5000
+print("Center is %s & course is %s & the fees of course is %d"%(s,course,fees))
+
+#Q6:-Let's do some interesting exercise:
+
+name="Tony Stark"
+salary=1000000
+print(" %s %d "%(name,salary))
+
+
+
